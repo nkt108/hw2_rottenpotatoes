4ce4474b9004e0916e493728c77e9bc38a67971d
Nate Tyler <nkt108@gmail.com>
2012-03-08 12:17:43 -0800

    Made modifications for part 3b

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params["sort"] == "title"
+      @movies = Movie.all(:order => "title")
+      @sort = "title"
+    elsif params["sort"] == "release_date"
+      @movies = Movie.all(:order => "release_date")
+      @sort = "release_date"
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
